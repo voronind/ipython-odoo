5f12eba49d465c3dcdbd4695e45ca86fac3a7863
Dmitry Voronin <dimka665@gmail.com>
2019-11-27 19:12:04 +0400

    Add %h magic

--- ipython_odoo/tables.py
+++ ipython_odoo/tables.py
@@ -1,11 +1,16 @@
 # coding=utf8
-
-from collections import OrderedDict, defaultdict
+import re
+import inspect
+from collections import OrderedDict
 
 from odoo.models import LOG_ACCESS_COLUMNS, Model
+from odoo.fields import Field
+
+import baron
+import redbaron
+from texttable import Texttable
 
 from IPython.core.magic import Magics, magics_class, line_magic, cell_magic, line_cell_magic
-
 
 
 def vals(model):
@@ -100,13 +105,122 @@ def get_field_names(fields):
         field_names[field_name] = field_detailed_name(field)
 
     pinned_field_names = OrderedDict({'__xml_id__': '__xml_id__'})
-    pinned_field_names.update((pinned_field_name, field_names.pop(pinned_field_name))
+    pinned_field_names.update((pinned_field_name, field_names.pop(pinned_field_name, ''))
                               for pinned_field_name in PINNED_FIELD_NAMES)
 
     pinned_field_names.update(field_names)
 
     return pinned_field_names
 
+
+def prepare_function(func):
+    func_source = inspect.getsource(func)
+
+    # Decrease indent
+    func_source_lines = [line for line in func_source.splitlines() if line.strip()]
+    min_indent_count = min(len(re.match('^ *', line).group()) for line in func_source_lines)
+    func_source = '\n'.join(line[min_indent_count:].rstrip() for line in func_source_lines)
+
+    try:
+        red = redbaron.RedBaron(func_source)
+    except baron.parser.ParsingError:
+        if '@api.returns' in func_source:
+            return '-@api.returns-'
+        else:
+            return '-parsing error-'
+
+    func_def = red[0]
+    assert func_def.type == 'def'
+
+    func_body = func_def.value
+
+    # Remove docstring
+    first_line = func_body[0]
+    if first_line.type == 'string' and first_line.name is None:
+        del func_body[0]
+
+    min_indent_count = min(len(line.indentation) for line in func_body)
+    func_body.decrease_indentation(min_indent_count)
+
+    func_body_source = func_body.dumps()
+
+    if len(func_body_source) < 180:
+        return func_body_source
+    else:
+        return u'✓'
+
+
+def get_model_attrs(records):
+    models = [klass for klass in records.__class__.__mro__ if Model in klass.__bases__]
+
+    IGNORE_ATTRS = {'_module', '__module__', '_inherit', '_order', '_name', '_description', '__doc__',
+                    '_local_constraints', '_local_sql_constraints',}
+    models.reverse()
+
+    # Headers
+    attrs = [[''] + [model._module for model in models]]
+
+    names = set()
+    for model in models:
+        model_attr_names = model.__dict__.keys()
+        model_attr_names.sort()
+
+        for name in model_attr_names:
+            if name in names:
+                continue
+
+            if name in IGNORE_ATTRS:
+                continue
+
+            attrs.append([name] + [mdl.__dict__.get(name) for mdl in models])
+            names.add(name)
+
+    return attrs
+
+def prepare_model_attrs(attrs):
+
+    row_count = len(attrs)
+    col_count = len(attrs[0])
+
+    for col in xrange(col_count):
+        for row in xrange(row_count):
+            attr = attrs[row][col]
+
+            if isinstance(attr, Field):
+                attrs[row][col] = attr.__class__.__name__
+
+            elif attr is None:
+                attrs[row][col] = u''
+
+            elif callable(attr):
+                # attrs[row][col] = u'✓'
+                attrs[row][col] = prepare_function(attr)
+
+    return attrs
+
+
+def print_model_attrs(attrs):
+    table = Texttable()
+
+    table.set_deco(Texttable.HEADER | Texttable.VLINES | Texttable.HLINES)
+    table.set_max_width(180)
+
+    table.add_rows(attrs)
+
+    print(table.draw())
+
+# Python AST
+# https://github.com/PyCQA/redbaron         Preferred
+# https://github.com/python-rope/rope
+
+# Для вывода таблиц в консоль есть модули   Last release    Stars
+# https://pypi.org/project/texttable/       2019            200
+# https://pypi.org/project/tabulate/        2019            100
+# https://pypi.org/project/termtables/      2019            15
+# https://pypi.org/project/tableprint/      2018            100
+# https://pypi.org/project/terminaltables/  2016            600
+# https://pypi.org/project/PrettyTable/     2013
+# https://pypi.org/project/asciitable/      2011
 
 @magics_class
 class MyMagics(Magics):
@@ -139,3 +253,12 @@ class MyMagics(Magics):
 
         pad_table(table)
         print_table(table)
+
+
+    @line_magic
+    def h(self, line):
+        records = eval(line, self.shell.user_ns)
+
+        attrs = get_model_attrs(records)
+        prepare_model_attrs(attrs)
+        print_model_attrs(attrs)
