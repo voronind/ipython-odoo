9b65ddd32da6e64a7175b2b2f6740e2fdde80ff6
Dmitry Voronin <dimka665@gmail.com>
2020-01-31 16:12:29 +0400

    procurement rules

--- ipython_odoo/procurmenet_rules.py
+++ ipython_odoo/procurmenet_rules.py
@@ -83,52 +83,75 @@ def search_suitable_rule(product=None, location=None, warehouse=None):
     return result
 
 
-def warehouse_rules(warehouse_or_company):
+#######################
+def selectable(route):
+    result = []
+
+    if route.sale_selectable:
+        result.append('sale')
+
+    if route.product_selectable:
+        result.append('product')
+
+    if route.product_categ_selectable:
+        result.append('category')
+
+    if route.warehouse_selectable:
+        result.append('warehouse')
+
+    return u','.join(result)
+
+
+def route_name(route):
+    if not route:
+        return False
+
+    return u'{route.sequence: 2d}. {route.name} ({route.id}) [{selectable}], [{wh}]'.format(
+        route=route, selectable=selectable(route), wh=route.warehouse_ids)
+
+
+def warehouse_rules(warehouse):
     from collections import OrderedDict
     from odoo.osv import expression
 
     result = OrderedDict()
 
-    if warehouse_or_company._name == 'stock.warehouse':
-        warehouse_id = warehouse_or_company.id
-    elif warehouse_or_company._name == 'res.company':
-        warehouse_id = env['res.company'].search([('company_id', '=', warehouse_or_company.id)], limit=1).id
-    else:
-        raise TypeError('Arg must be Warehouse or Company')
+    if isinstance(warehouse, int):
+        warehouse = env['stock.warehouse'].browse(warehouse)
 
+    elif warehouse._name == 'res.company':
+        warehouse = env['res.company'].search([('company_id', '=', warehouse.id)], limit=1)
 
-    warehouse_id =
-    rules = env['procurement.rules'].search([('warehouse_id', '=', )])
+    assert warehouse._name == 'stock.warehouse'
 
-    location = location or env['stock.location']
-    warehouse = warehouse or env['stock.warehouse']
+    rules = env['procurement.rule'].search([
+        ('warehouse_id', '=', warehouse.id),
+    ])
 
-    domain = [('location_id', 'in', parent_locations(location).ids)]
+    for rule in rules.sorted(lambda r: (r.location_id.id, r.route_sequence if r.route_sequence else 999, r.sequence)):
+        location = u'{}'.format(rule.location_id.name)
+        route = route_name(rule.route_id)
+        rule_str = u'{: 2d}. {} ({})'.format(rule.sequence, rule.name, rule.id)
 
-    if warehouse:
-        domain = expression.AND([['|', ('warehouse_id', '=', warehouse.id), ('warehouse_id', '=', False)], domain])
+        if location not in result:
+            result[location] = OrderedDict()
 
-    Pull = env['procurement.rule']
+        if route not in result[location]:
+            result[location][route] = []
 
-    if product:
-        product_routes = product.route_ids | product.categ_id.total_route_ids
-        if product_routes:
-            result['Product'] = product_res = OrderedDict()
-            for route in product_routes.sorted('sequence'):
-                product_rules = Pull.search(expression.AND([[('route_id', 'in', route.ids)], domain]), order='route_sequence, sequence')
-                if product_rules:
-                    product_res[route] = product_rules
-
-    warehouse_routes = warehouse.route_ids
-    if warehouse_routes:
-        result['Warehouse'] = warehouse_res = OrderedDict()
-        for route in warehouse_routes.sorted('sequence'):
-            warehouse_rules = Pull.search(expression.AND([[('route_id', 'in', route.ids)], domain]), order='route_sequence, sequence')
-            if warehouse_rules:
-                warehouse_res[route] = warehouse_rules
-
-    global_rules = Pull.search(expression.AND([[('route_id', '=', False)], domain]), order='sequence')
-    if global_rules:
-        result['Global'] = global_rules
+        result[location][route].append(rule_str)
 
     return result
+
+
+def prules(warehouse):
+    result = warehouse_rules(warehouse)
+
+    for location, routes in result.items():
+        print ' ' * 0, location
+        for route, rules in routes.items():
+            print ' ' * 4, route
+            for rule in rules:
+                print ' ' * 8, rule
+            print
+        print
