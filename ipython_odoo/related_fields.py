05f0639e55b0cfd0f7ec82e8c0448040bef1bc46
Dmitry Voronin <dimka665@gmail.com>
2020-11-12 12:03:14 +0400

    wip

--- ipython_odoo/related_fields.py
+++ ipython_odoo/related_fields.py
@@ -5,6 +5,28 @@ def merge(model, replaceents):
     pass
 
 
+def refferencing_records(env, recordset):
+
+    many2x_fields = env['ir.model.fields'].search([
+        ('ttype', 'in', ['many2one', 'many2many']),
+        ('relation', '=', recordset._name),
+        ('store', '=', True),
+    ])
+
+    many2x_fields = many2x_fields.filtered(lambda f: f.model in env
+                                                     and f.name in env[f.model]._fields)
+
+    result = []
+    for field in many2x_fields.sorted(lambda x: (x.model, x.name)):
+        rel_rs = env[field.model].with_context(active_test=False).search([
+            (field.name, 'in', recordset.ids),
+        ])
+        if rel_rs:
+            result.append((field.model, field.name, rel_rs))
+
+    return result
+
+
 def related_fields(line, user_ns):
     env = user_ns['env']
 
@@ -15,16 +37,4 @@ def related_fields(line, user_ns):
         if not recordset:
             raise error
 
-    many2x_fields = env['ir.model.fields'].search([
-        ('ttype', 'in', ['many2one', 'many2many']),
-        ('relation', '=', recordset._name),
-        ('store', '=', True),
-    ])
-
-    many2x_fields = many2x_fields.filtered(lambda f: f.model in env and f.name in env[f.model]._fields)
-
-    for field in many2x_fields.sorted(lambda x: (x.model, x.name)):
-        rel_rs = env[field.model].with_context(active_test=False).search([
-            (field.name, 'in', recordset.ids),
-        ])
-        print field.model, field.name, rel_rs
+    return refferencing_records(env, recordset)
