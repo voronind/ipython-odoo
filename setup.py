edb3d572d8e1b15d2e9c7cff5de9fc0b35ba05a8
Dmitry Voronin <dimka665@gmail.com>
2021-01-19 15:58:29 +0400

    Bump version to 0.3

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
 setup(
-    version='0.2.3',
+    version='0.3',
     install_requires=[
         'ipython',
         'redbaron',
         'texttable',
     ],
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+    ]
 )
